b88831414135b616eb1ab19787beda4c3b64a49f
ElvishPiper <81609085+ElvishPiper@users.noreply.github.com>
2022-01-28 00:25:40 +0800

    Update db_config.php

--- CP5310 Web project/db_config.php
+++ CP5310 Web project/db_config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', "localhost");
-define('DB_USERNAME', "root");
-define('DB_PASSWORD', "");
-define('DB_NAME', 'cp5310 website');
+define('DB_HOST', "ec2-54-208-139-247.compute-1.amazonaws.com");
+define('DB_USERNAME', "efegskwqhbbixq");
+define('DB_PASSWORD', "26b378989f0ca5939d616ea0a57ea3895f2538a16ca3bd12fa7263bab9b5cf1c");
+define('DB_NAME', 'dcer3mi5l06jg5');
 
 $con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if (!$con) {
